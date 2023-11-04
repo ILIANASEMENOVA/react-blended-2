a52d7dd667d6dca849e160b115a55f07a88ee116
y-hrubskyi <y.hrubskyi@gmail.com>
2023-11-04 11:40:48 +0200

    add fetch func

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -6,17 +6,51 @@ import { Button, SearchForm, Grid, GridItem, Text, CardItem } from 'components';
 export class Gallery extends Component {
   state = {
     query: '',
+    images: [],
+    page: 1,
+    isEmpty: false,
   };
 
+  async componentDidUpdate(prevProps, prevState) {
+    const { query, page } = this.state;
+    const prevQuery = prevState.query;
+    const prevPage = prevState.page;
+
+    if (prevQuery == query && prevPage === page) {
+      return;
+    }
+
+    const { photos, total_results } = await ImageService.getImages(query, page);
+    if (!total_results) {
+      this.setState({ isEmpty: true });
+      return;
+    }
+
+    this.setState(prevState => ({ images: [...prevState.images, ...photos] }));
+  }
+
   handleSubmit = query => {
-    console.log(query);
-    this.setState({ query });
+    this.setState({ query, images: [], page: 1, isEmpty: false });
   };
+
   render() {
+    const { images, page, isEmpty } = this.state;
+
     return (
       <>
         <SearchForm handleSubmit={this.handleSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <Grid>
+          {images.map(({ id, avg_color, alt, src }) => (
+            <GridItem key={id}>
+              <CardItem color={avg_color}>
+                <img src={src.large} alt={alt} />
+              </CardItem>
+            </GridItem>
+          ))}
+        </Grid>
+        {isEmpty && (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
       </>
     );
   }
